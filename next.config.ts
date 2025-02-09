047b531b3102b52ccf665e3d12199af770287126
Carina Rey <carinarey@Carinas-MacBook-Air.local>
2025-02-09 14:28:07 -0500

    social services component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+    i18n: {
+      locales: ["en", "es"],
+      defaultLocale: "en", 
+    },
+  
 };
+
 
 export default nextConfig;

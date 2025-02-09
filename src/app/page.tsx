e28eaaf1ed5ca6d634f3a296fc066c44a46ea06e
Carina Rey <carinarey@Carinas-MacBook-Air.local>
2025-02-09 16:58:02 -0500

    splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,67 @@
+"use client";
+import { useEffect, useState } from "react";
+//import Header from "./components/header";
+import Image from "next/image";
 import Header from "./components/header";
+import SearchBar from "./components/searchBar";
+import Link from "next/link";
 
-export default async function Home() {
+export default function Home() {
+  const [showImage, setShowImage] = useState(true);
+  const [, setSearchQuery] = useState("");
+
+  const handleSearch = (query: string) => {
+    console.log("Searching for category: ", query);
+    setSearchQuery(query);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowImage(false);
+    }, 4000); // Show image for 4 seconds
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
   return (
-    <div>
-      <Header />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-        <h1>HELLO!!!!!</h1>
-
-
-      </div>
-    </div>
+    <>
+      {showImage ? (
+        <div className="flex justify-center items-center min-h-screen bg-white">
+          <div className=" flex flex-col items-center justify-center min-h-screen gap-4 text-center">
+            <Image
+              src="/mercadoSplash.png"
+              alt="Splash Screen"
+              width={170}
+              height={145}
+            />
+            <Image
+              src="/Mercado.png"
+              alt="Splash Screen"
+              width={370}
+              height={78}
+              className="mt=4"
+            />
+            <p>Su seguridad en NY, primero</p>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Header />
+          <div className="flex flex-col gap-5 justify-center items-center font-montserrat]">
+            <SearchBar onSearch={handleSearch} placeholder="Search category" />
+            <Link href="" target="_blank" className="flex items-center">
+              {" "}
+              <Image
+                src="/SecurityCard.png"
+                alt="Splash Screen"
+                width={390}
+                height={72}
+                className="mt=4"
+              />
+            </Link>
+            <h1 className="text-xl items-start">Categories</h1>
+          </div>
+        </>
+      )}
+    </>
   );
 }
